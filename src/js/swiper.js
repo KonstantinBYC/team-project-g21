806f9733150239eeace7b871ad93f7b5d0d73836
Danylo Kliushnychenko <danylktv2@gmail.com>
2023-04-07 16:42:05 +0100

    Reviews section
    Додав свій swiper.js, але import не спрацював, тому приєднав script`ом в index.html і також приєднав link у верхній частині html.

---
+++ src/js/swiper.js
@@ -0,0 +1,27 @@
+const views = new Swiper('.reviews__block', {
+  loop: true,
+  direction: 'horizontal',
+  mousewheel: true,
+  keyboard: true,
+
+  pagination: {
+    el: '.reviews__swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      /* slidesPerGroup: 3, */
+      spaceBetween: 28,
+    },
+    768: {
+      slidesPerView: 2,
+      /* slidesPerGroup: 2, */
+      spaceBetween: 16,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  },
+});
