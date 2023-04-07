9e6e93f6b49a0e1ff4ec553a2cdc3776a2e77b3e
KonstantinBYC <globalmacintosh@gmail.com>
2023-04-07 14:14:21 +0300

    edit 07-04

--- src/js/main.js
+++
@@ -1,4 +0,0 @@
-import './js/mobile-meny.js';
-import './js/modal.js';
-import './js/script.js';
-import './js/slick.min.js';
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './js/mobile-menu';
+import './js/modal';
+import './js/script';
+import './js/slick.min-menu';
