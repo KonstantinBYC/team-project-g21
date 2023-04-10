1d141375a2f708adf89872158e81a495849e3ab8
KonstantinBYC <globalmacintosh@gmail.com>
2023-04-10 04:25:04 +0300

    Edit1 10-04-23

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,29 @@
 const views = new Swiper('.our-products__block', {
-	loop: true,
-	direction: 'horizontal',
-	mousewheel: true,
-	keyboard: true,
-  
-	pagination: {
-	  el: '.our-products__swiper-pagination',
-	  clickable: true,
-	},
-	breakpoints: {
-	  1200: {
-		slidesPerView: 3.91,
-	  
-		spaceBetween: 18,
-	  },
-	  768: {
-		slidesPerView: 2.5,
-	  
-		spaceBetween: 18,
-	  },
-	  320: {
-		slidesPerView: 1,
-		spaceBetween: 20,
-	  },
-	},
-  });
-  
-  
+  loop: true,
+  direction: 'horizontal',
+  mousewheel: true,
+  keyboard: true,
+  simulateTouch: false,
+
+  pagination: {
+    el: '.our_products__swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 4,
+
+      spaceBetween: 18,
+    },
+    768: {
+      slidesPerView: 2.5,
+
+      spaceBetween: 18,
+    },
+    320: {
+      slidesPerView: 2,
+
+      spaceBetween: 40,
+    },
+  },
+});
